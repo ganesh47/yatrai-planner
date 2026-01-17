62f113e2c3519e2db17eb4d38e1cf3b6f7e18419
ganesh47 <22994026+ganesh47@users.noreply.github.com>
2026-01-17 13:01:00 +0530

    baseline

---
+++ YatraiPlannerTests/YatraiPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  YatraiPlannerTests.swift
+//  YatraiPlannerTests
+//
+//  Created by Ganesh Raman on 17/01/26.
+//
+
+import Testing
+@testable import YatraiPlanner
+
+struct YatraiPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
